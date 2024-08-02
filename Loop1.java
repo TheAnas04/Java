4710ed371fa17367a80002c667849033f11b0638
TheAnas04 <04anasshaikh04@gmail.com>
2024-08-02 21:33:28 +0530

    for loop

---
+++ Loop1.java
@@ -0,0 +1,12 @@
+/*
+    02/08/2024
+    USE FOR LOOP AND PRINT NUMBERS 1-10 
+*/
+class Loop1 {
+    public static void main(String args[]){
+        int i;
+        for ( i=1 ; i<=10 ; i++ ){
+            System.out.print(i+"  ");
+        }
+    }
+}
