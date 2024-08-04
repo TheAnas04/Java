52c8360912815ad0ccbae9fccd80e9fcd8f56cdb
TheAnas04 <04anasshaikh04@gmail.com>
2024-08-04 19:35:34 +0530

    While Loop Syntax

---
+++ While1.java
@@ -0,0 +1,13 @@
+/*
+    04/08/2024
+    Use of wile loop
+*/
+class While1 {
+    public static void main(String[] args) {
+        int num=1;
+        while( num<=10 ){
+            System.out.print(num+"  ");
+            num++;
+        }
+    }
+}
