c418ec6969dc2d3eb3eb26b750db9645060d0e7e
TheAnas04 <04anasshaikh04@gmail.com>
2024-08-06 16:33:32 +0530

    Do-While Syntax

---
+++ DoWhile.java
@@ -0,0 +1,13 @@
+/*
+    06/08/2024
+    Use of Do-While loop
+*/
+class DoWhile {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.print(i+"  ");
+            i++;
+        }while(i<=10);
+    }
+}
