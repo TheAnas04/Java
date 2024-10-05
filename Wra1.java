2db872affb00dfe0d4abf86d63763b6ce5601dac
TheAnas04 <04anasshaikh04@gmail.com>
2024-10-05 15:31:27 +0530

    Wrapper Class

---
+++ Wra1.java
@@ -0,0 +1,13 @@
+/*
+    05/10/2024
+    Wrapper class
+*/
+
+class Wra1 {
+    public static void main(String[] args) {
+        Integer a = 10 ;
+        System.out.println(a);
+        a = null;
+        System.out.println(a);
+    }
+}
