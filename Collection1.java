998684da8a3859ac0aedd105c168777ff8a04731
TheAnas04 <04anasshaikh04@gmail.com>
2024-10-07 14:54:42 +0530

    ArrayList add operation

---
+++ Collection1.java
@@ -0,0 +1,18 @@
+/*
+    07/10/2024
+    Collection
+*/
+import java.util.ArrayList;
+class Collection1 {
+    public static void main(String args[]){
+        ArrayList colour = new ArrayList<>();
+        colour.add("Red");
+        colour.add("Blue");
+        colour.add("Green");
+        colour.add("Yellow");
+        colour.add("White");
+        colour.add("Black");
+
+        System.out.println(colour);
+    }
+}
