2c34e7ea8270f993b978c1e93710c741faf12a9c
TheAnas04 <04anasshaikh04@gmail.com>
2024-10-14 14:00:41 +0530

    Example of set

---
+++ Collection4.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+class Collection4 {
+    public static void main(String[] args) {
+        HashSet city = new HashSet<>();
+        city.add("Madrid");
+        city.add("Manchester");
+        city.add("Paris");
+        city.add("Indore");
+        city.add("Paris");
+
+        System.out.println(city);
+    }
+}
